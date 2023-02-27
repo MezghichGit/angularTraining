c7a2005f9e21ce200c3e5131424e530a8935a4f2
moahmed amine mezghich <git@mezghich.com>
2023-02-27 12:19:07 +0100

    provider service

---
+++ src/app/services/provider.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService {
+
+  // injection de dependences : HttpClient
+  constructor(private http: HttpClient) { }
+
+  //1)get Providers
+  getProviders() {
+    return this.http.get("http://127.0.0.1:8080/providers/list");
+  }
+
+  //2) add provider
+  addProvider(provider:any) { }
+
+  //3) get provider
+  getProvider(id:number) { }
+
+  //4) add provider
+  deleteProvider(id:number) { }
+
+  //5) add provider
+  updateProvider(id:number, new_provider:any) { }
+}
