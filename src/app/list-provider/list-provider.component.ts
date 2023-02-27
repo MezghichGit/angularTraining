44f13911440390c3c930d6a135d349a6e51a1863
moahmed amine mezghich <git@mezghich.com>
2023-02-27 12:32:24 +0100

    list des providers

--- src/app/list-provider/list-provider.component.ts
+++ src/app/list-provider/list-provider.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { ProviderService } from '../services/provider.service';
 @Component({
   selector: 'app-list-provider',
   templateUrl: './list-provider.component.html',
   styleUrls: ['./list-provider.component.css']
 })
 export class ListProviderComponent {
+
+  providers:any;
+  // injection de dependences : ProviderService
+  constructor(private providerService: ProviderService) { }
+
+  ngOnInit(): void {
+     this.providerService.getProviders().subscribe(
+      data =>{
+        this.providers = data;
+        console.log(this.providers);
+      }
+     );
+  }
+
 
 }
